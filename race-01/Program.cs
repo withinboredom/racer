90bf06b4f0134d83e528e3315e27c3561363ba83
Rob Landers <landers.robert@gmail.com>
2022-03-16 23:59:36 +0100

    initial commit

---
+++ race-01/Program.cs
@@ -0,0 +1,16 @@
+﻿using System.Diagnostics;
+
+var action = () =>
+{
+    var counter = 0;
+
+    var watch = Stopwatch.StartNew();
+    for (var i = 0; i < 4000000; i++)
+    {
+        ++counter;
+    }
+    watch.Stop();
+
+    Console.WriteLine($"Counted to {counter} in {watch.Elapsed.TotalMilliseconds} milliseconds", counter, watch.ElapsedMilliseconds);
+};
+action.Invoke();
---
+++ race-02/Program.cs
@@ -0,0 +1,36 @@
+﻿using System.Diagnostics;
+using System.Text;
+
+var test = () => {
+    using var file = File.OpenText("/file/file.bin");
+    var counter = 0;
+    var sw = Stopwatch.StartNew();
+    while(!file.EndOfStream)
+    {
+        if(file.Read() == '1')
+        {
+            counter++;
+        }
+    }
+    sw.Stop();
+    Console.WriteLine($"Counted {counter} 1s in {sw.Elapsed.TotalMilliseconds} milliseconds");
+};
+
+test();
+
+var testAsync = async () =>
+{
+    using var sourceStream = new FileStream("/file/file.bin", FileMode.Open, FileAccess.Read, FileShare.Read, 4096, true);
+    var counter = 0;
+    byte[] buffer = new byte[4096];
+    var numRead = 0;
+    var sw = Stopwatch.StartNew();
+    while ((numRead = await sourceStream.ReadAsync(buffer, 0, buffer.Length)) != 0)
+    {
+        counter += buffer.Take(numRead).Count((x) => x == Encoding.UTF8.GetBytes("1")[0]);
+    }
+    sw.Stop();
+    Console.WriteLine($"Counted {counter:N} 1s in {sw.Elapsed.TotalMilliseconds} milliseconds");
+};
+
+//await testAsync();
